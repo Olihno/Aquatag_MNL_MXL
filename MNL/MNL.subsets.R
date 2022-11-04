e067789bd924728abd924eaf7b78d915b0525f99
Oliver Becker <oliver.becker89@gmail.com>
2022-11-04 21:55:18 +0100

    Initialize filies

---
+++ MNL/MNL.subsets.R
@@ -0,0 +1,11 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+#   MNLs on random subsets of the global choice set using SRS and strategic 
+#   sampling (AQUATAG)
+#
+#   Oliver Becker (oliver@pksb.de)
+#   3th November 2022
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
